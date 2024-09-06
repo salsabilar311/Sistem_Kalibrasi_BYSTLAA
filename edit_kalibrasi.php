27229dfb2fa2288bdd64b916f0ba8bd3fe959fb4
salsabilar311 <salsabilar311@gmail.com>
2024-09-06 16:13:03 +0700

    add edit kalibrasi

--- edit_kalibrasi.php
+++ edit_kalibrasi.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Kalibrasi - BYSTLAA</title>
-    <!-- DESAIN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
@@ -15,7 +14,7 @@
         <div class="sidebar" id="side_nav">
           <div class="header-box px-2 pt-3 pb-4">
             <img src="assets/img/image 1.svg" alt="" />
-            <!-- <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i class="fal fa-stream"></i></button> -->
+            <!-- <a class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i class="fal fa-stream"></i></a> -->
           </div>
 
           <div class="menu">
@@ -48,11 +47,11 @@
             <div class="container-fluid">
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <form class="d-flex ms-auto p-2" role="profile">
-                  <a class="nav-link dropdown-toggle" style="align-items: end;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <a class="nav-link dropdown-toggle" style="align-items: end;" href="#" role="a" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fas fa-user-circle"></i>
                   </a>
 
-                  <ul class="dropdown-menu nav-item dropdown-menu-end m-1">
+                  <ul class="dropdown-menu nav-item dropdown-menu-end m-2">
                     <li><a class="dropdown-item" href="#">Profile</a></li>
                     <li><a class="dropdown-item" href="#">Setting</a></li>
                     <li><hr class="dropdown-divider" /></li>
@@ -64,54 +63,43 @@
           </nav>
 
           <div class="container-fluid p-4">
-            <div class="row mt-2">
-              <div class="col-4"><h4>Data Kalibrasi BYSTLAA</h4></div>
+            <h4>Progres Kalibrasi BYSTLAA</h4>
 
-              <div class="col-8" style="text-align: right;">
-                <button class="btn btn-primary">Input Kalibrasi</button>
-              </div>
-            </div>
-            
-            <!-- form kalibrasi -->
+              <!-- table view progres kalibrasi -->
               <div class="row">
-                <div class="card p-0 m-2">           
+                <div class="card col-12 p-0 m-2" style="width: 99%;">
                   <div class="card-body">
-                    <table class="table-bordered table-sm fs-6" style="width: 100%">
+                    <table class="table-bordered table-sm fs-6 p-0" style="width: 100%;" >
                       <thead class="text-white bg-dark text-center">
-                        <!-- header tabel -->
                         <tr>
+                          <td>No</td>
                           <td>No. Order</td>
-                          <td>Nama Alat</td>
-                          <td>Merk</td>
-                          <td>Tipe</td>
-                          <td>No. Seri</td>
-                          <td>Asal</td>
+                          <td>Pemilik</td>
                           <td>Tanggal Kalibrasi</td>
+                          <td>Keterangan</td>
                           <td>Action</td>
                         </tr>
                       </thead>
-                      <!-- isi tabel -->
-                      <tbody>
-                        <td>No. Order</td>
-                        <td>Nama Alat</td>
-                        <td>Merk</td>
-                        <td>Tipe</td>
-                        <td>No. Seri</td>
-                        <td>Asal</td>
-                        <td>Tanggal Kalibrasi</td>
+                      <tbody  class="text-center">
+                        <tr>
+                        <td>1</td>
+                        <td>12345</td>
+                        <td>Balai Yasa</td>
+                        <td>11/9/2023</td>
+                        <td>-</td>
                         <td>
                           <div class="btn p-0">
-                            <a href="edit_kalibrasi.php" class="btn btn-secondary">Edit</a>
-                            <a href="edit.php" class="btn btn-danger">Delete</a>
-                            <a href="delete.php" class="btn btn-info" >Detail</a>
-                          </div>
+                            <a href="edit.php" class="btn btn-info">Detail</a>
+                            <a href="delete.php" class="btn btn-danger">Delete</a>
+                        </div>
                         </td>
+                        </tr>
                       </tbody>
                     </table>
                   </div>
+                </div>
               </div>
-            </div>
-            
+              
           </div>
           
       </div>
@@ -127,4 +115,4 @@
       });
     </script>
   </body>
-</html>
+    </html>
